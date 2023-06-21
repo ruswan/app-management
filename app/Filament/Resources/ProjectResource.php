e40a54ec862b050116e203e533a3777298052e2c
ruswan <sukabumikode@gmail.com>
2023-06-21 12:02:47 +0700

    Change icons

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -22,7 +22,7 @@ class ProjectResource extends Resource
 {
     protected static ?string $model = Project::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-briefcase';
 
     public static function form(Form $form): Form
     {
