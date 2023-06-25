7cf58aad6474e0368542a925f69ee135a388eca0
Ruswan <sukabumikode@gmail.com>
2023-06-25 08:41:32 +0700

    add project_type_id to projects table

---
+++ database/migrations/2023_06_25_013700_add_project_type_id_to_projects_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreignId('project_type_id')->after('production_year_id')->references('id')->on('project_types')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeign('projects_project_type_id_foreign');
+            $table->dropColumn('project_type_id');
+        });
+    }
+};
