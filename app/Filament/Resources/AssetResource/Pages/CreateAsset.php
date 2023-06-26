0cec53d4822490c40c0aeaf6cf76b60764626a68
ruswan <sukabumikode@gmail.com>
2023-06-26 14:12:15 +0700

    mutateFormDataBeforeCreate

--- app/Filament/Resources/AssetResource/Pages/CreateAsset.php
+++ app/Filament/Resources/AssetResource/Pages/CreateAsset.php
@@ -9,4 +9,16 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateAsset extends CreateRecord
 {
     protected static string $resource = AssetResource::class;
+
+    /**
+     * Mutates the form data before creating a new record.
+     *
+     * @param array<string, int|string|null> $data The form data to be mutated.
+     * @return array<string, int|string|null> The mutated form data.
+     */
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['owner_id'] = auth()->id();
+        return $data;
+    }
 }
