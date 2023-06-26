8110772d9ca2d70063a925776a22b26041836fea
ruswan <sukabumikode@gmail.com>
2023-06-26 13:31:26 +0700

    Add asset category resource

---
+++ app/Filament/Resources/AssetCategoryResource.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AssetCategoryResource\Pages;
+use App\Filament\Resources\AssetCategoryResource\RelationManagers;
+use App\Models\AssetCategory;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AssetCategoryResource extends Resource
+{
+    protected static ?string $model = AssetCategory::class;
+
+    protected static ?string $navigationGroup = "Data Master";
+
+    protected static ?string $navigationIcon = 'heroicon-o-folder';
+
+    protected static ?int $navigationSort = 100;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->maxLength(255),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+                Tables\Actions\ForceDeleteBulkAction::make(),
+                Tables\Actions\RestoreBulkAction::make(),
+            ]);
+    }
+
+    /**
+     * Mendapatkan daftar kelas pengelola relasi yang terkait dengan objek saat ini.
+     *
+     * @return array<class-string> Array yang berisi daftar kelas Relation Manager yang terkait.
+     */
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Mengembalikan array yang berisi daftar halaman (pages) yang terkait.
+     *
+     * @return array<string, array<mixed>> The array of pages.
+     */
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAssetCategories::route('/'),
+            'create' => Pages\CreateAssetCategory::route('/create'),
+            'view' => Pages\ViewAssetCategory::route('/{record}'),
+            'edit' => Pages\EditAssetCategory::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
