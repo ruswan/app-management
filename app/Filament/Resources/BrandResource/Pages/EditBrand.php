28aa150b22a0d7d99ac25c1f709923cd4c39d255
ruswan <sukabumikode@gmail.com>
2023-06-26 14:04:32 +0700

    Add branch resource

---
+++ app/Filament/Resources/BrandResource/Pages/EditBrand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\BrandResource\Pages;
+
+use App\Filament\Resources\BrandResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBrand extends EditRecord
+{
+    protected static string $resource = BrandResource::class;
+
+    /**
+     * Mendapatkan daftar objek aksi yang tersedia untuk digunakan.
+     *
+     * @return array<Actions\Action> Daftar objek aksi.
+     */
+    protected function getActions(): array
+    {
+        return [
+            Actions\ViewAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make(),
+            Actions\RestoreAction::make(),
+        ];
+    }
+}
